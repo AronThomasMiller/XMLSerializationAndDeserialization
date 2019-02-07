6baf7db2865b5616b8946e0e27c2dda811ea1aa4
AronThomasMiller <AronThomasMiller@gmail.com>
2019-02-07 22:12:09 +0200

    first commit

---
+++ ConsoleApp51/Hotel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp51
+{
+    [Serializable]
+    public class Hotel
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public int Price
+        {
+            get;
+            set;
+        }
+        public int Rating
+        {
+            get;
+            set;
+        }
+        public Hotel(string Name, int Price, int Rating)
+        {
+            this.Name = Name;
+            this.Price = Price;
+            this.Rating = Rating;
+        }
+        public Hotel()
+        {
+        }
+    }
+
+}
+
---
+++ ConsoleApp51/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleApp51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hotel Mariya = new Hotel("Mariya", 2000, 3);
+            Hotel Bykovuna = new Hotel("Bykvuna", 5000, 5);
+            Hotel Kavkaz = new Hotel("Kavkaz", 4000, 4);
+            Hotel Laxivka = new Hotel("Laxivka", 500, 1);
+            Hotel[] arr = new Hotel[] { Mariya, Bykovuna, Kavkaz, Laxivka };
+            //Serialization
+            XmlSerializer Serializer = new XmlSerializer(typeof(Hotel[]));
+            using (FileStream fs = new FileStream("Hotel.xml", FileMode.OpenOrCreate))
+            {
+                Serializer.Serialize(fs, arr);
+                Console.WriteLine("Ми сериализнули 4 готели");
+            }
+            //Deserialization
+            using (FileStream fs = new FileStream("Hotel.xml", FileMode.OpenOrCreate))
+            {
+                Hotel[] array = (Hotel[])Serializer.Deserialize(fs);
+                foreach (var hotelchik in array)
+                {
+                    Console.WriteLine(hotelchik.Name + hotelchik.Price + hotelchik.Rating);
+                }
+            }
+        }
+    }
+}
